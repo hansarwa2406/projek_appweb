10f3318af2fb53875110239b1ab2f501bc0631a8
haynis <75485602+haynis@users.noreply.github.com>
2021-01-05 17:05:06 +0700

    Update view_acara.php

--- view_acara.php
+++ view_acara.php
@@ -12,9 +12,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Data Acara</title>
+		<title>Data Anggota</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/gaya.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -24,7 +24,7 @@
 	<body>
 		<header>
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			  <a class="navbar-brand" href="view_anggota.php">Event Manager</a>
+			  <a class="navbar-brand" href="index.php">Event Manager</a>
 			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 			    <span class="navbar-toggler-icon"></span>
 			  </button>
@@ -34,14 +34,17 @@
 			      <li class="nav-item">
 			        <a class="nav-link" href="index.php">Home</a>
 			      </li>
-			      <li class="nav-item">
-			        <a class="nav-link" href="view_anggota.php">Data Anggota</a>
-			      </li>
 			      <li class="nav-item active">
-			        <a class="nav-link" href="view_acara.php">Data Acara <span class="sr-only">(current)</span></a>
+			        <a class="nav-link" href="view_anggota.php">Data Anggota </a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="view_acara.php">Data Acara <span class="sr-only">(current)</span> </a>
 			      </li>
 			      <li class="nav-item">
 			        <a class="nav-link" href="view_divisi.php">Data Divisi</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="view_jobdeks.php">Data Jobdeks</a>
 			      </li>
 			      <li class="nav-item">
 			        <a class="nav-link" href="logout.php">Logout</a>
@@ -55,11 +58,13 @@
 			<div class="card altaboutus altcard" id="aboutus">
 			  <center><div class="card-body alteraboutus">
 			    <section>
-			    	<h5 class="card-title">Data acara</h5>
 
-			    	<table class="table" style="text-align: center;">
+			    	<h5 class="card-title">Data Acara</h5>
+
+			    	<table class="table" style="text-align: center; display: inline-block; overflow: auto;">
 			    	  <thead>
 			    	    <tr>
+
 			    	      <th scope="col">ID acara</th>
 			    	      <th scope="col">Nama Acara</th>
 			    	      <th scope="col">Tipe Acara</th>
@@ -89,7 +94,6 @@
 			    	      <td><?php echo $row['lokasi']; ?></td>
 			    	      <td><?php echo $row['start_date']; ?></td>
 			    	      <td><?php echo $row['due_date']; ?></td>
-			    	   
 							<td>
 								<?php
 									echo "<a href='edit_acara.php?id=$row[id_acara]'>Edit</a>";
@@ -125,3 +129,5 @@
 		</footer>
 	</body>
 </html>
+
+
